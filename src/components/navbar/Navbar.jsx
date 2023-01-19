94ed2c65da15be349ec2d94913545f7a27761043
Seamus Ahkit <seamusahkit@Seamuss-MacBook-Pro.local>
2023-01-19 21:32:07 +1300

    start navbar + add styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
 import './navbar.css';
+
+// BEM -> Block element modifier
 
 const Navbar = () => {
   return (
-    <div>
-        Navbar
+    <div className='gpt3__navbar'>
+        <div className='gpt3__navbar-links'>
+          <div className='gpt3__navbar-links_logo'>
+            <img src={logo} alt='logo' />
+          </div>
+          <div className='gpt3_navbar-links_container'>
+            <p><a href=''></a></p>
+          </div>
+        </div>
     </div>
   )
 }
